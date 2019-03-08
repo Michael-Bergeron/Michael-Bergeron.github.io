7dd556b5c6fa643919174985c5f4a2665fb01b6a
StealthLSU <45839261+StealthLSU@users.noreply.github.com>
2019-03-07 20:50:54 -0600

    changed scale back to 20

--- snake/snake.js
+++ snake/snake.js
@@ -3,7 +3,7 @@
   const startGame = document.getElementById('start');
   const scoreUpdate = document.getElementById('score');
   var ctx = canvas.getContext('2d');
-  var scale = 10;
+  var scale = 20;
   var speed = 200;
   var rows = canvas.width/scale;
   var rows = canvas.height/scale;
